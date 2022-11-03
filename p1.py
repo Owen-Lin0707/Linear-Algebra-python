a2f00a7c07e87fdb3653c62cb36d2d63417234f3
Owen-Lin0707 <108745138+Owen-Lin0707@users.noreply.github.com>
2022-11-03 21:06:48 +0800

    HW2

--- p1.py
+++ p1.py
@@ -1,1 +1,56 @@
-p1_main(student_id)
+import numpy as np
+
+def decode(cipher, key):
+  cipher_list = []
+  for word in cipher:
+    if word >= 'A' and word <= 'Z':
+      cipher_list.append(ord(word) - 65)
+    elif word == '_':
+      cipher_list.append(26)
+    elif word == '.':
+      cipher_list.append(27)
+    elif word == ',':
+      cipher_list.append(28)
+    elif word == '?':
+      cipher_list.append(29)
+    elif word == '!':
+      cipher_list.append(30)
+
+  length = len(cipher_list)
+  if length % 3 == 1:
+    cipher_list.append(cipher_list[length - 1])
+    cipher_list.append(cipher_list[length - 1])
+  if length % 3 == 2:
+    cipher_list.append(cipher_list[length - 1])
+
+  arr_c = np.array(cipher_list)
+  arr_c = np.reshape(arr_c, (-1, 3)).T
+
+  key_slist = key.split(' ')
+  key_list = []
+  for s in key_slist:
+    key_list.append(int(s))
+
+  arr_a = np.array(key_list)
+  arr_a = np.reshape(arr_a, (3, 3))
+  
+  arr_p = np.dot(mod_inv(arr_a), arr_c).T
+  arr_p = np.ravel(arr_p)
+
+  plain = ''
+  for num in arr_p:
+    num = num % 31
+    if num >= 0 and num <= 25:
+      plain += chr(num + 65)
+    elif num == 26:
+      plain += '_'
+    elif num == 27:
+      plain += '.'
+    elif num == 28:
+      plain += ','
+    elif num == 29:
+      plain += '?'
+    elif num == 30:
+      plain += '!'
+      
+  return plain
--- p2.py
+++ p2.py
@@ -1,1 +1,64 @@
-p2_main(student_id)
+import numpy as np
+
+def get_key(cipher, plain):
+  cipher_list = []
+  for word in cipher:
+    if word >= 'A' and word <= 'Z':
+      cipher_list.append(ord(word) - 65)
+    elif word == '_':
+      cipher_list.append(26)
+    elif word == '.':
+      cipher_list.append(27)
+    elif word == ',':
+      cipher_list.append(28)
+    elif word == '?':
+      cipher_list.append(29)
+    elif word == '!':
+      cipher_list.append(30)
+
+  length = len(cipher_list)
+  if length % 3 == 1:
+    cipher_list.append(cipher_list[length - 1])
+    cipher_list.append(cipher_list[length - 1])
+  if length % 3 == 2:
+    cipher_list.append(cipher_list[length - 1])
+  
+  arr_c = np.array(cipher_list)
+  arr_c = np.reshape(arr_c, (-1, 3)).T
+
+  plain_list = []
+  for word in plain:
+    if word >= 'A' and word <= 'Z':
+      plain_list.append(ord(word) - 65)
+    elif word == '_':
+      plain_list.append(26)
+    elif word == '.':
+      plain_list.append(27)
+    elif word == ',':
+      plain_list.append(28)
+    elif word == '?':
+      plain_list.append(29)
+    elif word == '!':
+      plain_list.append(30)
+
+  length = len(plain_list)
+  if length % 3 == 1:
+    plain_list.append(plain_list[length - 1])
+    plain_list.append(plain_list[length - 1])
+  if length % 3 == 2:
+    plain_list.append(plain_list[length - 1])
+  
+  arr_p = np.array(plain_list)
+  arr_p = np.reshape(arr_p, (-1, 3)).T
+
+  arr_a = np.dot(arr_c, mod_inv(arr_p))
+  arr_a = np.ravel(arr_a)
+
+  key = ''
+  for i in range(9):
+    arr_a[i] = arr_a[i] % 31
+    key += str(arr_a[i])
+    if i != 8:
+      key += ' '
+      
+  return key
